e46d48799dcc2b2008e95acbd3f29f09d39b4326
manuel leppert <manuel.leppert@thoughtworks.com>
2022-11-13 17:39:11 +0100

    expand bulletlist: can detect bullet list lines

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,18 +1,40 @@
 local api = vim.api
 
--- markdown extend
+-- helper functions
 
-local mdGroup = api.nvim_create_augroup("ExpandMarkdown", { clear = true})
-local isLineBulletList = function ()
+local is_bullet_list = function(cur_line)
+    for i = 1, #cur_line do
+        local c = cur_line:sub(i, i)
+        if c == '*' then
+            if cur_line:sub(i + 1, i + 1) == ' ' then
+                print(cur_line)
+                return
+            end
+        else
+            if c ~= ' ' then
+                return false
+            end
+        end
+    end
+    return false
+end
+
+
+
+local mdGroup = api.nvim_create_augroup("ExpandMarkdown", { clear = true })
+local isLineBulletList = function()
     local cur_line = api.nvim_get_current_line()
-    print(cur_line)
+    if not is_bullet_list(cur_line)
+    then
+        return
+    end
 end
 
 
 api.nvim_create_autocmd("CursorMoved", {
-    pattern = { "*.md"},
---    event = { 'CursorMoved' },
---    command = "silent! ",
+    pattern = { "*.md" },
+    --    event = { 'CursorMoved' },
+    --    command = "silent! ",
     callback = isLineBulletList,
     group = mdGroup
 })
